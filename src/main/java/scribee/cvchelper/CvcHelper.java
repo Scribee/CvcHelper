580cc7e277193fc2862812decc9cc3938186eab1
Scribee <c1sloan6@gmail.com>
2020-09-28 22:38:14 -0700

    Update version numbers

--- src/main/java/scribee/cvchelper/CvcHelper.java
+++ src/main/java/scribee/cvchelper/CvcHelper.java
@@ -19,7 +19,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 @Mod(modid = CvcHelper.MODID, version = CvcHelper.VERSION)
 public class CvcHelper {
     public static final String MODID = "cvchelper";
-    public static final String VERSION = "0.1";
+    public static final String VERSION = "1.0";
     
     // Section symbol used by minecraft for text formatting
 	private static final String S = String.valueOf('\u00a7');
